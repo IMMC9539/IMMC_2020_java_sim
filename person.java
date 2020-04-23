f9afd0d9e2877c478dc827cd8d6eb6528270a6dd
IMMC9539 <64113992+IMMC9539@users.noreply.github.com>
2020-04-22 21:23:09 -0400

    Add files via upload
    person.java file has the pieces of code from the visibility/dropping equations put into it as methods, but doesn't have them implemented yet.

--- person.java
+++ person.java
@@ -22,7 +22,7 @@ public class person
 		 * if we can implement the actual percent chances and the distance from the cash registar
 		 * and all, this will be a much stronger model.
 		 */
-		double probability = (int)(Math.random()*6);
+		double probability = (Math.random());
 		//buy the item
 		if (probability==0||probability==1||probability==2)
 		{
@@ -39,6 +39,57 @@ public class person
 			//nothing
 		}
 	}
+	
+	/* The whole probability of dropping implementation is the following: 
+	 */
+	
+	//Calculates the probability of products being roughly handled in a given department.
+	public double rough_handling_score(double total_perimeter, double visible_perimeter, double population_density, double product_density, double max_ratio, double v_weight, double ppp_weight)
+	{
+		double ppp = people_per_product_metric(population_density, product_density, max_ratio);
+		double visibility = cashier_visibility(total_perimeter, visible_perimeter);
+		double score = ppp * ppp_weight + visibility * v_weight;
+		return score;
+	}
+	
+	public double opening_packaging_score(double population_density, double product_density, double popularity_score, double visible_perimeter, double total_perimeter, double ppp_weight, double ps_weight, double v_weight, double max_ppp, double max_ratio)
+	{
+		double ppp = people_per_product_metric(population_density, product_density, max_ratio);
+		double vp = cashier_visibility(visible_perimeter, total_perimeter);
+		double score = ppp_weight*ppp + ps_weight*popularity_score + v_weight*vp;
+		return score;
+	}
+	
+	//Calculates a score to represent the probability of product falling or being dropped in a given department.
+	public double dropping_score(double population_density, double product_density, double max_ratio)
+	{
+//		Calculates a score to represent the probability of product falling or being dropped in a given department.
+//		param population_density
+//		param product_density
+//		param max_ratio
+//		return
+				  
+		return people_per_product_metric(population_density, product_density, max_ratio);
+	}
+	
+	//People per product
+	public double people_per_product(double population_density, double product_density)
+	{
+		  return population_density/product_density;
+	}
+
+	//visibility given perimeter and visible perimeter
+	public double cashier_visibility(double total_perimeter, double visible_perimeter)
+	{
+		  return visible_perimeter/total_perimeter;
+	}
+
+	//People per product metric
+	public double people_per_product_metric(double population_density, double product_density, double max_ratio)
+	{
+		  return population_density/(product_density*max_ratio);
+	}	
+	
 	
 	public void moveForward(map currentMap)
 	{
